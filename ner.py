a049386b823ba228a05fe9a87a12038be79c5604
aashnajena <32356795+aashnajena@users.noreply.github.com>
2019-09-29 01:35:51 +0530

    Update ner.py

--- ner.py
+++ ner.py
@@ -3,17 +3,49 @@ from spacy import displacy
 from collections import Counter
 import en_core_web_sm
 import nltk
+import json
 
 nlp = en_core_web_sm.load()
 
-f = ['Firstly, I would like to say this']
+selected = 0
+no = 0
+# e = open('chem.txt','r')
+
+f = open('kech103.txt','r')
+l = [] 
+# for line in e:
+# 	l.append(line.split('.')[0])
+# print(len(l))
+
+s1 = []
 for line in f:
 	sent = nltk.sent_tokenize(line)
-	for l in sent:
-		print(l)
-		l = nlp(l.rstrip())
-		print([(X.text, X.label_) for X in l.ents] )
-		for token in l:
-		    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-		            token.shape_, token.is_alpha, token.is_stop)
+	for s in sent:
+		flag=0
+		s = s.strip()
+		s = ' '.join(s.split('\t'))
+		s = nlp(s.strip())
+		for token in s:
+			if (token.lemma_=='be' or token.tag_== 'VBZ' or token.tag_ == 'VB' or token.tag_ == 'VBP') and len(s.ents)>0:
+				print(s)
+				print([(X.text, X.label_) for X in s.ents] )
+				flag=1
+				selected +=1
+				s1.append(s)
+				break
+		if flag==0:
+			no += 1	
+print(selected)
+print(no)
+
+
+# common = 0
+# for sent in s1:
+# 	if sent in l:
+# 		print('common:')
+# 		print(sent)
+# 		common+=1
+
+# print(common)		
+
 
